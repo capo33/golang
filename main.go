1e6273e7cb99fc0c6cd1e18036b077cf5754320b
Mohamed <s2000588@edu.bc.fi>
2023-11-05 22:49:52 +0200

    work with int, bits and float

--- main.go
+++ main.go
@@ -3,14 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	var nameOne string = "John"
-	var nameTwo = "Sally"
-	var nameThree string
+	// // Strings
+	// var nameOne string = "John"
+	// var nameTwo = "Sally"
+	// var nameThree string
 
- 	fmt.Println(nameOne, nameTwo, nameThree)
+ 	// fmt.Println(nameOne, nameTwo, nameThree)
 
-	nameOne = "Michael"
-	nameThree = "Jennifer"
+	// nameOne = "Michael"
+	// nameThree = "Jennifer"
 
-	fmt.Println(nameOne, nameTwo, nameThree)
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// Integers
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40 
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// Bits & Memory
+	var numOne int8 = 25
+	var numTwo int8 = -128
+	var numThree uint = 25
+
+	fmt.Println(numOne, numTwo, numThree)
+
+	// Floats
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 356789.123456789
+	scoreThree := 1.5
+
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
+
 }
